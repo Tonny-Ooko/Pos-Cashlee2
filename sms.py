6ff4c202f092d41fc149f73ecb5993ac590bd825
Tosh <138869956+Tonny-Ooko@users.noreply.github.com>
2025-02-19 21:55:14 +0200

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,13 +1,6 @@
 from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 
-account_sid = 'ACe140beae8b884f7056dc35b0ef15c7b4'
-auth_token = '77377fb40f7f363a600aaf0127d451aa'
-client = Client(account_sid, auth_token)
-
-twilio_phone_number = '+254 705 598269'
-destination_phone_number = '+1 972-360-9222'
-message_body = 'Activate'
 
 try:
     message = client.messages.create(
